e926cb9daecb02976f3dbdaf397b316328d479dd
Quintin Adam <quintinadam@live.com>
2015-05-20 10:52:21 -0600

    removes unused columns in migration

--- db/migrate/20150519181814_add_spree_school_invoice_checkouts.rb
+++ db/migrate/20150519181814_add_spree_school_invoice_checkouts.rb
@@ -3,10 +3,7 @@ class AddSpreeSchoolInvoiceCheckouts < ActiveRecord::Migration
     create_table :spree_invoice_school_checkouts do |t|
       t.string :tax_id_number
       t.string :school_name
-      t.string :payer_id
       t.string :state, defualt: "completed"
-      t.datetime :refunded_at
-      t.string :refund_type
 
       t.timestamps null: false
     end
