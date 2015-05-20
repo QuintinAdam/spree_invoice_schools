4471c40a377dfddb2c6b8db6a3ff6254705cae7b
Quintin Adam <quintinadam@live.com>
2015-05-20 10:50:57 -0600

    adds source now at checkout

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -1,15 +1,42 @@
 module Spree
   CheckoutController.class_eval do
-    before_action :create_source_for_school_invoice, only: [:update], if: :checking_out_useing_school_invoice?
+    # Copies the update method
+    def update
+      if @order.update_from_params(params, permitted_checkout_attributes, request.headers.env)
+
+        # other thing different
+        create_source_for_school_invoice if checking_out_useing_school_invoice?
+
+        @order.temporary_address = !params[:save_user_address]
+        unless @order.next
+          flash[:error] = @order.errors.full_messages.join("\n")
+          redirect_to checkout_state_path(@order.state) and return
+        end
+
+        if @order.completed?
+          @current_order = nil
+          flash.notice = Spree.t(:order_processed_successfully)
+          flash['order_completed'] = true
+          redirect_to completion_route
+        else
+          redirect_to checkout_state_path(@order.state)
+        end
+      else
+        render :edit
+      end
+    end
 
     private
 
     def checking_out_useing_school_invoice?
-      params['payment_source'] && params['order']['payments_attributes'][0]['payment_method_id'].try(:to_i) == Spree::PaymentMethod.find_by_type('Spree::PaymentMethod::InvoiceSchool').id
+      params[:state] == "payment" && params['order']['payments_attributes'][0]['payment_method_id'].try(:to_i) == @invoice_payment = Spree::PaymentMethod.find_by_type('Spree::PaymentMethod::InvoiceSchool').id
     end
 
     def create_source_for_school_invoice
-      # raise 'the roof'
+      if newest_payment = @order.payments.last
+        newest_payment.source = InvoiceSchoolCheckout.create(school_name: params['order']['payments_attributes'][0]['source_attributes']['school_name'], tax_id_number: params['order']['payments_attributes'][0]['source_attributes']['tax_id_number'])
+        newest_payment.save
+      end
     end
   end
 end
